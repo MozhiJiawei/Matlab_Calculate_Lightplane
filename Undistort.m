f60a1ad804c63b1c9a847c7c21a550601db38952
Mozhi Jiawei <qetwe0000@gmail.com>
2016-04-27 14:36:07 +0800

    using fullfile to create filename

--- Undistort.m
+++ Undistort.m
@@ -1,13 +1,13 @@
 function Undistort(pic_num)
 %UNTITLED2 此处显示有关此函数的摘要
 %   此处显示详细说明
-filePathIn = [pwd,'\pic_input\lightimages\'];
-filePathOut = [pwd,'\pic_input\undistortimages\'];
+filePathOut = fullfile(pwd,'pic_input','undistortimages');
 for i = 1:1:pic_num 
-  imageFileNames{i} = [filePathIn, num2str(i), '.bmp'];
+  imageFileNames{i} = fullfile(pwd,'pic_input','lightimages',...
+      sprintf('%d.bmp',i));
 end
 
-load('calibrationSession_firt_try.mat')
+load('calibrationSession.mat')
 if( ~exist(filePathOut))
   mkdir(filePathOut);
 end
@@ -17,7 +17,7 @@ for i=1:1:pic_num
   undistortedImage = undistortImage(originalImage, ...
     calibrationSession.CameraParameters);
     
-  imagename = sprintf('%s\\%d.bmp',filePathOut,i);
+  imagename = fullfile(filePathOut, sprintf('%d.bmp',i));
   imwrite(undistortedImage, imagename);
 end
 
